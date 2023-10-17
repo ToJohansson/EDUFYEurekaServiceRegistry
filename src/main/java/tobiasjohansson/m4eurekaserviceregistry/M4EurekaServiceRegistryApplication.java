f8f72867a45abafa97f19d22106e209b11e10f17
Tobias Johansson <tobiasp91@hotmail.com>
2023-10-17 12:07:05 +0200

    propertis is set

--- src/main/java/tobiasjohansson/m4eurekaserviceregistry/M4EurekaServiceRegistryApplication.java
+++ src/main/java/tobiasjohansson/m4eurekaserviceregistry/M4EurekaServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package tobiasjohansson.m4eurekaserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class M4EurekaServiceRegistryApplication {
 
 	public static void main(String[] args) {
