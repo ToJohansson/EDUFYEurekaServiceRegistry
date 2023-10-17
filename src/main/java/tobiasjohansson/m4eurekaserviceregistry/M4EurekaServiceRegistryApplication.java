104a4cbd3b2fcbc73662765affe58b07b71be7df
Tobias Johansson <tobiasp91@hotmail.com>
2023-10-17 10:46:43 +0200

    Initial commit

---
+++ src/main/java/tobiasjohansson/m4eurekaserviceregistry/M4EurekaServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package tobiasjohansson.m4eurekaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4EurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M4EurekaServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tobiasjohansson/m4eurekaserviceregistry/M4EurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package tobiasjohansson.m4eurekaserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M4EurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
